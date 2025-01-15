398ca7a2f09cd86675f7c2250002a79525f70f67
Meerim K <thvrim@gmail.com>
2025-01-15 13:56:59 +0600

    изменены конфиги для деплоя

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
@@ -11,25 +11,25 @@ import EditQuestion from "./pages/EditQuestion";
 import NotFound from "./pages/NotFound";
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/polechudes" element={<Main />} />
-        <Route path="/auth" element={<Login />} />
-        <Route path="/admin/create-question" element={<CreateQuestion />} />
-        <Route path="/admin/edit-question/:id" element={<EditQuestion />} />
-        <Route path="*" element={<NotFound />} />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <Admin />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/auth" element={<Login />} />
+                <Route path="/admin/create-question" element={<CreateQuestion />} />
+                <Route path="/admin/edit-question/:id" element={<EditQuestion />} />
+                <Route path="*" element={<NotFound />} />
+                <Route
+                    path="/admin"
+                    element={
+                        <ProtectedRoute>
+                            <Admin />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
